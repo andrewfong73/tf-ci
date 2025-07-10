8da5f306c3b99b2ffb62b7f1256ef7f5ebd4da45
afong <an2fong@gmail.com>
2025-07-10 21:38:03 +0800

    hello

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "andrewfong"  #Change this
+    key    = "andrewfong" #Change this
     region = "ap-southeast-1"
   }
 }
